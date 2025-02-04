e7a438641beb1ac75a3ea115b9587048f4abfaaa
Aly <el7amrawy@outlook.com>
2025-02-04 17:08:22 +0200

    change store name

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { type ReactNode } from "react";
+import { type Metadata } from "next";
+import { DraftModeNotification } from "@/ui/components/DraftModeNotification";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: {
+		default: "IFROF",
+		template: "%s | IFROF"
+	},
+	description: "IFROF - Your Fashion Destination",
+	metadataBase: process.env.NEXT_PUBLIC_STOREFRONT_URL
+		? new URL(process.env.NEXT_PUBLIC_STOREFRONT_URL)
+		: undefined,
+	icons: {
+		icon: "/favicon.ico",
+		shortcut: "/favicon.ico",
+		apple: "/apple-touch-icon.png",
+	},
+};
+
+export default function RootLayout(props: { children: ReactNode }) {
+	const { children } = props;
+
+	return (
+		<html lang="en" className="min-h-dvh">
+			<body className={`${inter.className} min-h-dvh`}>
+				{children}
+				<DraftModeNotification />
+			</body>
+		</html>
+	);
+}
