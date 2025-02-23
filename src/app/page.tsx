a17928f0ad8a27dc44dd32a9e566e563580a1854
Aly <el7amrawy@outlook.com>
2025-02-23 03:13:48 +0200

    add / before channel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function EmptyPage() {
-  const targetUrl = process.env.NEXT_CHANNEL_REDIRECT_URL || "/default-channel";
+  const targetUrl = "/"+( process.env.NEXT_CHANNEL_REDIRECT_URL || "default-channel");
   redirect(targetUrl);
 }
